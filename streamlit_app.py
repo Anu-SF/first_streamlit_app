f0aadb57eecf6faea6ac25f2625e451e29fe50b2
Anu-SF <109796949+Anu-SF@users.noreply.github.com>
2022-07-22 16:13:19 +0530

    Update streamlit_app.py
    Added pictures

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3  & Blueberry Omatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free Rag Egg')
+streamlit.text('🥣 Omega3  & Blueberry Omatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free Rag Egg')
+streamlit.text('🥑🍞 Avacado Toast')
