caa9f939bb8116337949ec823c188594e5d79bb7
Anu-SF <109796949+Anu-SF@users.noreply.github.com>
2022-07-28 20:33:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,13 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+#streamlit.text(fruityvice_response.json())
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 insecure_mode=True
 client_session_keep_alive=True
