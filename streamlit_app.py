3e3dca65d624316bdd4abf20fc124ef7217534d4
Anu-SF <109796949+Anu-SF@users.noreply.github.com>
2022-07-29 11:14:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,8 @@ try:
     back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 except URLError as e:
- streamlit.error()
+    streamlit.error()
   
-import requests
-
-#streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-
-# write your own comment - what does this do?
-
-
 fruit_choice1 = streamlit.text_input('What would you like to add?','jackfruit')
 streamlit.write('The user entered ', fruit_choice1)
 fruityvice_response1 = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice1)
